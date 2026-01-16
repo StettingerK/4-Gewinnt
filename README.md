9203a52caf1270025e8fd80976240e3b0acf25d2
Kevin Stettinger <Kevin.Stettinger@rdf.nuernberg.de>
2026-01-16 11:20:25 +0100

    anfang

---
+++ README.md
@@ -0,0 +1,9 @@
+Funktionen Gui.py
+    - ausgabe(inp:str)
+    - print_spielfeld(spielfeld:dict[list[str]])
+    - spielzug() -> int
+    - spieler_abfragen() -> list[str]
+
+Funktion Main.py
+    - gewinner_ermitteln(spielfeld:dict[list[str]]) -> bool
+    - unentschieden_ermitteln(spielfeld:dict[list[str]]) -> bool
--- gui.py
+++ gui.py
@@ -1,4 +1,14 @@
+from rich import print
 
+def generiere_spielfeld():
+    print(farbe("x"))
+
+def farbe(zeichen: str) -> str:
+    if zeichen == "x":
+        return "[bold red]X[/bold red]"
+    if zeichen == "0":
+        return "[bold blue]O[/bold blue]"
+    return " "
 
 if __name__ == "__main__":
-    pass
+    generiere_spielfeld()
